1074b3c67d290b5555fab45875672e3cd0c02b67
Taha Eid <10812022101005@stud.cu.edu.eg>
2025-04-12 18:08:51 +0200

    Add files via upload

--- screens/AuthApp.jsx
+++ screens/AuthApp.jsx
@@ -1,70 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { Alert, ScrollView, StyleSheet } from 'react-native';
-import { onAuthStateChanged, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword } from '@firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { View, ActivityIndicator } from 'react-native';
+import { onAuthStateChanged } from '@firebase/auth';
 import { auth } from '../config/firebase';
-import AuthScreen from './AuthScreen';
 import AuthenticatedScreen from './AuthenticatedScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './LoginScreen';
+import RegisterScreen from './RegisterScreen';
+
+const Stack = createStackNavigator();
 
 const AuthApp = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
-  const [isLogin, setIsLogin] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user);
+      setLoading(false);
     });
 
     return () => unsubscribe();
   }, []);
 
-  const handleAuthentication = async () => {
-    try {
-      if (user) {
-        await signOut(auth);
-        Alert('User logged out successfully!');
-      } else {
-        if (isLogin) {
-          await signInWithEmailAndPassword(auth, email, password);
-          Alert('User signed in successfully!');
-        } else {
-          await createUserWithEmailAndPassword(auth, email, password);
-          Alert('User created successfully!');
-        }
-      }
-    } catch (error) {
-      Alert('Authentication error:', error.message);
-    }
-  };
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 
-  return (
-    <ScrollView contentContainerStyle={styles.container}>
-      {user ? (
-        <AuthenticatedScreen user={user} handleAuthentication={handleAuthentication} />
-      ) : (
-        <AuthScreen
-          email={email}
-          setEmail={setEmail}
-          password={password}
-          setPassword={setPassword}
-          isLogin={isLogin}
-          setIsLogin={setIsLogin}
-          handleAuthentication={handleAuthentication}
-        />
-      )}
-    </ScrollView>
+  return user ? (
+    <AuthenticatedScreen user={user} />
+  ) : (
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16,
-    backgroundColor: '#f0f0f0',
-  },
-});
 
 export default AuthApp;
