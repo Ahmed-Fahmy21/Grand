6b4d6e8b24faf56913a19be69520914f07eff7e6
Marwan-Essam55 <marawan.elbob@gmail.com>
2025-05-05 00:59:17 +0300

    Update _layout.jsx

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,39 +1,55 @@
 import { Tabs } from 'expo-router';
 import { FontAwesome } from '@expo/vector-icons';
+import { Image } from 'react-native';
 
 export default function Layout() {
   return (
-    <Tabs>
-      <Tabs.Screen
-        name="index" 
-        options={{
-          title: "Home",
-          headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name="home" size={24} color={color} />
-          ),
-        }}
+      <Tabs>
+        <Tabs.Screen
+  name="index"
+  options={{
+    title: "Home",
+    headerShown: false,
+    tabBarIcon: ({ color }) => (
+      <Image
+        source={require('../assets/logo.jpeg')}
+        style={{ width: 26, height: 26 }}
+        resizeMode="contain"
       />
-      <Tabs.Screen
-        name="about" 
-        options={{
-          title: "About",
-          headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name="info" size={24} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="rooms" 
-        options={{
-          title: "Rooms",
-          headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name="bed" size={24} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    ),
+  }}
+/>
+        <Tabs.Screen
+          name="about"
+          options={{
+            title: "About",
+            headerShown: false,
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="info" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="auth"
+          options={{
+            title: "Auth",
+            headerShown: false,
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="user" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="rooms"
+          options={{
+            title: "Rooms",
+            headerShown: false,
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="bed" size={24} color={color} />
+            ),
+          }}
+        />
+        
+      </Tabs>
   );
 }
